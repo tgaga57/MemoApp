16cc475649cc61790e507653c93638f6280179a0
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2019-08-03 15:49:53 +0900

    delatePlus

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController,UITextViewDelegate  {
         super.viewDidLoad()
         
         
+        
+        
         // textViewがUITextViewを使えるように
         textView.delegate = self
         // AppDelegateを参照する為の定数。
@@ -24,15 +26,28 @@ class ViewController: UIViewController,UITextViewDelegate  {
         // AppDelegateに定義したlastTextを参照し、textViewに格納する。
         textView.text = appDelegate.lastText
     }
+    
+    //削除ボタン
+    @IBAction func deleteButton(_ sender: Any) {
+        textView.text = ""
+        saveText()
+        
+    }
     //TextViewに何か入力されたら動作します
     func textViewDidChange(_ textView: UITextView) {
-        // AppDelegateを呼び出して変数に格納します
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        // AppDelegateに記述した"lastTextに入力されている内容を格納します。
-        appDelegate.lastText = textView.text
+        saveText()
+    }
+        //端末にデータを保存する
+        func saveText() {
+            // AppDelegateを呼び出して変数に格納します
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            // AppDelegateに記述した"lastTextに入力されている内容を格納します。
+            appDelegate.lastText = textView.text
+            
+        }
         
     }
     
-}
+
 
